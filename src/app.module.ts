4ba3457e823fa27bc4082ea70a55873fd9d1d0c2
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-09-08 20:54:30 -0300

    setting up config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: [`.env.stage.${process.env.STAGE}`] }),
     TasksModule,
     AuthModule,
     TypeOrmModule.forRoot({
